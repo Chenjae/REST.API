4667c90540020a5605f9d24b8a94de4671b9290f
Chenjae <injea95@naver.com>
2021-10-25 17:33:39 +0900

    21-10-25 수업 내용
    JWT 인증, REST API

--- src/main/java/com/mycompany/webapp/security/WebSecurityConfig.java
+++ src/main/java/com/mycompany/webapp/security/WebSecurityConfig.java
@@ -12,9 +12,14 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -42,6 +47,20 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers("/board/**").authenticated()
 			.antMatchers("/**").permitAll();
 		
+		//세션 비활성화
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		
+		//JwtCheckFilter 추가
+		//id password를 제공하는 것이 아니라 Jwt객체를 전달하기 때문에
+		//id, password로 인증을 확인하는 Filter전에 추가되어야 한다.
+		JwtCheckFilter jwtCheckFilter = new JwtCheckFilter();
+		http.addFilterBefore(jwtCheckFilter, UsernamePasswordAuthenticationFilter.class);
+	
+		//CORS 설정 교차 출처 리소스 공유 활성화
+		//http.authorizeRequests().antMatchers("/api/**").authenticated();
+		//http.addFilterBefore(apiCheckFilter(), UsernamePasswordAuthenticationFilter.class);
+		//http.addFilterBefore(apiLoginFilter(), UsernamePasswordAuthenticationFilter.class);
+		http.cors();
 	}	
 	
 	@Override
@@ -86,6 +105,22 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		RoleHierarchyImpl roleHierarchyImpl = new RoleHierarchyImpl();
 		roleHierarchyImpl.setHierarchy("ROLE_ADMIN > ROLE_MANAGER > ROLE_USER");
 		return roleHierarchyImpl;
+	}
+	
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		//모든 요청 사이트 허용
+		configuration.addAllowedOrigin("*");
+		//모든 요청 방식 허용
+		configuration.addAllowedMethod("*");
+		//모든 요청 헤더 허용
+		configuration.addAllowedHeader("*");
+		
+		//모든 URL 요청에 대해서 위 내용을 적용
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
 	}
 			
 }
